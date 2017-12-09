055d9349a3938dd4d7c8edcc449d842475eadacd
Daniel Johansson <danijoh@gmail.com>
2017-12-09 14:55:03 +0100

    commit stuff for real this time

---
+++ TestData/TestData/Fetcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestData
+{
+	//fetches stuff from the web...
+	class Fetcher
+	{
+	}
+}
