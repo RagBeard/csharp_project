2f72be08233b2cfc5c538971253ae20164988a9c
Daniel Johansson <danijoh@gmail.com>
2017-12-11 16:08:03 +0100

    first attempt at SQL database interface, reading JSON and putting it into SQL. Dataset not included.

---
+++ TestData/TestData/DBConnection/DBConnection.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TestData
+{
+	// interface to SQL Database
+
+	public class DBConnection
+	{
+		string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\HistoricalEvents.mdf;Integrated Security=True";
+
+		SqlConnection connection;
+		SqlDataReader reader;
+
+		public DBConnection()
+		{
+
+		}
+
+		public void TransactAll(List<SqlCommand> commands)
+		{			
+			using (connection = new SqlConnection(connectionString))
+			{
+
+				connection.Open();
+
+				var rows = 0;
+
+				foreach (var com in commands)
+				{
+					com.Connection = connection;
+
+					rows += com.ExecuteNonQuery();
+				}
+
+				Console.WriteLine("Performed Multiple queries, total rows affected: {0}", rows);
+			}
+		}
+
+
+		//public void TransactAll(List<string> commands)
+		//{
+		//	SqlCommand cmd = new SqlCommand();
+			
+		//	// DO PARAMETERS @ 
+
+		//	using (connection = new SqlConnection(connectionString))
+		//	{
+
+		//		connection.Open();
+
+		//		var rows = 0;
+
+		//		foreach (var com in commands)
+		//		{
+		//			cmd.CommandText = com;
+		//			cmd.CommandType = CommandType.Text;
+		//			cmd.CreateParameter();
+		//			cmd.Connection = connection;
+					
+		//			rows += cmd.ExecuteNonQuery();
+		//		}
+
+		//		Console.WriteLine("Performed Multiple queries, total rows affected: ", rows);
+
+		//	}
+		//}
+
+		public void Transact(string commandText)
+		{
+			SqlCommand cmd = new SqlCommand();
+
+			using (connection = new SqlConnection(connectionString))
+			{
+
+				connection.Open();
+
+				cmd.CommandText = commandText;
+				cmd.CommandType = CommandType.Text;
+				cmd.Connection = connection;
+
+
+				var rows = cmd.ExecuteNonQuery();
+
+				Console.WriteLine("Performing query: " + commandText);
+				Console.WriteLine("DB Transaction, rows affected: {0}", rows);				
+								
+			}
+		}
+
+		public void Query(string commandText)
+		{
+			SqlCommand cmd = new SqlCommand();
+
+			using (connection = new SqlConnection(connectionString))
+			{
+				cmd.CommandText = commandText;
+				cmd.CommandType = CommandType.Text;
+				cmd.Connection = connection;
+
+				connection.Open();
+
+				reader = cmd.ExecuteReader();
+
+				if (reader.HasRows)
+				{
+					while (reader.Read())
+					{
+						Console.WriteLine("{0}\t{1}\t{2}\t{3}", reader.GetInt32(0), reader.GetDateTime(1), reader.GetString(2), reader.GetString(3));
+					}
+				}
+				else
+				{
+					Console.WriteLine("No rows found.");
+				}
+
+				reader.Close();
+			}
+		}
+	}
+}
--- TestData/TestData/Program.cs
+++ TestData/TestData/Program.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
 
 namespace TestData
 {
 	class Program
 	{
+		static string dataRoot = @"F:\Projects\GitRepos\csharp_project\datasets\";
+
+		static string historyAD = "all_history_ad.json";
+		static string historyADvalid = "all_history_ad_valid.json";
+
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, Computer!");
-			Console.Beep();
+
+			DBConnection dbc = new DBConnection();
+
+			dbc.Query("SELECT * FROM Event;");
+
+
+			HistoryLoader loader = new HistoryLoader(dataRoot + historyADvalid);
+
+			loader.Load(dbc);
+
+
+
+			Console.Read();
 		}
+
+
 	}
+		
+
 }
