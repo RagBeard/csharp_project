11615c3a2f0292d8ee7600caac4d61d0d8bfbad5
Daniel Johansson <danijoh@gmail.com>
2017-12-12 15:51:20 +0100

    some tests for my JSON Loader

---
+++ TestData/TestDataTests/JSON/LoaderTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestData.JSON;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestData.JSON.Tests
+{
+	[TestClass()]
+	public class LoaderTests
+	{
+		private class TestClass
+		{
+			public string a { get; set; }
+			string b { get; set; }
+		}
+
+		private void LoadFromJSONTestFileNotFound()
+		{
+			//arrange
+			Loader loader = new Loader();
+			string path = @"F:\Projects\GitRepos\csharp_project\datasets\nofile";
+			TestClass tc = new TestClass();
+
+			//act
+			bool result = loader.LoadFromJSON<TestClass>(path, out tc);
+
+			//assert
+			Assert.IsNull(tc);
+			Assert.IsFalse(result);
+
+		}
+
+		private void LoadFromJSONTestArgumentException()
+		{
+			//arrange
+			Loader loader = new Loader();
+			string path = "";
+			TestClass tc = new TestClass();
+
+			//act
+			bool result = loader.LoadFromJSON<TestClass>(path, out tc);
+
+			//assert
+			Assert.IsNull(tc);
+			Assert.IsFalse(result);
+		}
+
+		private void LoadFromJSONTestInvalidJSON()
+		{
+			//arrange
+			Loader loader = new Loader();
+			string path = @"F:\Projects\GitRepos\csharp_project\datasets\invalid_json.json";
+			TestClass tc = new TestClass();
+
+			//act
+			bool result = loader.LoadFromJSON<TestClass>(path, out tc);
+
+			//assert
+			Assert.IsNull(tc);
+			Assert.IsFalse(result);
+		}
+
+		[TestMethod()]
+		public void LoadFromJSONTest()
+		{
+						
+			LoadFromJSONTestArgumentException();
+			LoadFromJSONTestFileNotFound();
+			LoadFromJSONTestInvalidJSON();
+
+		}
+	}
+}
